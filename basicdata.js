eda4309966419a957c761c9f1b10668a8cf49709
Srija Reddy <srijareddykoya@example.com>
2022-03-10 11:40:59 +0530

    added a file

---
+++ basicdata.js
@@ -0,0 +1,10 @@
+let empname ="srija";
+ export let branch = "hyd";
+ export let getBranch = function()
+{
+    return branch;
+}
+ export let setBranch = function(newBranch)
+{
+     branch= newBranch;
+}
---
+++ transfer.js
@@ -0,0 +1,4 @@
+import{ getBranch,setBranch } from "./basicdata.js";
+console.log(getBranch());
+setBranch("tirupati");
+console.log(getBranch());
